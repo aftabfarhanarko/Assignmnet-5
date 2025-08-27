4763489d9a4cb89fb4de53986520de82abd1aebc
Aftab Farhan <aftabfarhan324@gmail.com>
2025-08-27 15:05:12 +0600

    addes all buttone hover effect

--- script/index.js
+++ script/index.js
@@ -1,3 +1,62 @@
 // Heart click Event
 
-document.getElementsByClassName('').a
+// Fire Service Number Functions
+document.getElementById('fireService-btn').addEventListener('click', function () {
+    
+    alert( 'Calling Fire Service 102...')
+    
+    let points = parseInt(document.getElementById('point').innerText);
+     let ubdate = points - 20;
+    points.innerText = 
+    console.log(points);
+})
+// National Emergency Service Number Functions
+document.getElementById('national-btn').addEventListener('click', function () {
+    
+    alert( 'Calling National Emergency Service 999...')
+    
+    let points = parseInt(document.getElementById('point').innerText);
+     let ubdate = points - 20;
+    points.innerText = 
+    console.log(points);
+})
+// Polic Service Number Functions
+document.getElementById('polic-btn').addEventListener('click', function () {
+    
+    alert( 'Calling Polic Service 102...')
+    
+    let points = parseInt(document.getElementById('point').innerText);
+     let ubdate = points - 20;
+    points.innerText = 
+    console.log(points);
+})
+// Bangladesh Railway Service Number Functions
+document.getElementById('railway-btn').addEventListener('click', function () {
+    
+    alert( 'Calling Bangladesh Railway Service 131...')
+    
+    let points = parseInt(document.getElementById('point').innerText);
+     let ubdate = points - 20;
+    points.innerText = 
+    console.log(points);
+})
+// BRAC Bank Service Number Functions
+document.getElementById('brakBank-btn').addEventListener('click', function () {
+    
+    alert( 'Calling BRAC Bank Service 16221...')
+    
+    let points = parseInt(document.getElementById('point').innerText);
+     let ubdate = points - 20;
+    points.innerText = 
+    console.log(points);
+})
+// Ambulance Service Number Functions
+document.getElementById('ambulince-btn').addEventListener('click', function () {
+    
+    alert( 'Calling Ambulance Service 123456...')
+    
+    let points = parseInt(document.getElementById('point').innerText);
+     let ubdate = points - 20;
+    points.innerText = 
+    console.log(points);
+})
